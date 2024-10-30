86ccc7c66b8900e30d21a4ad3d00233d11c8d7ad
Analice <analice.leite12@gmail.com>
2024-10-30 13:33:32 -0300

    🔧 Adjustment update view

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,26 +1,31 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
-
-import git
+import subprocess
 
 @csrf_exempt
 def update(request):
     if request.method == "POST":
-        '''
-        pass the path of the diectory where your project will be
-        stored on PythonAnywhere in the git.Repo() as parameter.
-        Here the name of my directory is "test.pythonanywhere.com"
-        '''
-        repo = git.Repo('/home/Analice/bookstore')
-        origin = repo.remotes.origin
-
-        origin.pull()
-        return HttpResponse("Updated code on PythonAnywhere")
+        try:
+            # Defina o caminho para o seu repositório
+            repo_dir = '/home/Analice/bookstore'
+            
+            # Comando de git pull usando subprocess
+            result = subprocess.run(
+                ["git", "-C", repo_dir, "pull"],
+                check=True,
+                capture_output=True,
+                text=True
+            )
+            
+            # Retorne a saída do comando para verificar o sucesso
+            return HttpResponse(f"Updated code on PythonAnywhere:\n{result.stdout}")
+        except subprocess.CalledProcessError as e:
+            # Em caso de erro, capture a saída e mostre no response
+            return HttpResponse(f"Failed to update code:\n{e.stderr}")
     else:
         return HttpResponse("Couldn't update the code on PythonAnywhere")
 
-
 def hello_world(request):
-  template = loader.get_template('hello_world.html')
-  return HttpResponse(template.render())
+    template = loader.get_template('hello_world.html')
+    return HttpResponse(template.render())
